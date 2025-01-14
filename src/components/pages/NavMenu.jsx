c2fad55c439374bd50ebff03076dc7e8b3631bb4
tonimtnz <tonimartinezlluesma@gmail.com>
2025-01-14 10:38:55 +0100

    responsive like a pro

--- src/components/pages/NavMenu.jsx
+++ src/components/pages/NavMenu.jsx
@@ -1,15 +1,27 @@
 import { use } from "react";
 import { Arrow } from "../../../public/assets/Arrow";
+import { Home } from "../../../public/assets/Home";
+import { Folder } from "../../../public/assets/Folder";
+import { MeIcon } from "../../../public/assets/MeIcon";
 
 export function NavMenu(){
 
     return(
-        <section className="nav-father">
-            <Arrow></Arrow>
-            <div className="nav-cont">
-                <a href="/">Home</a>
-                <a href="/projects">Mis Proyectos</a>
-                <a href="/me">Sobre mi</a>
+        <section className="nav-menu">
+            <div className="nav-father">
+                <div className="nav-arrow">
+                    <Arrow></Arrow>
+                </div>
+                <div className="nav-cont">
+                    <a href="/">Home</a>
+                    <a href="/projects">Mis Proyectos</a>
+                    <a href="/me">Sobre mi</a>
+                </div>
+                <div className="nav-icons">
+                    <a href="/"><Home></Home></a>
+                    <a href="/projects"><Folder></Folder></a>
+                    <a href="/me"><MeIcon></MeIcon></a>
+                </div>
             </div>
         </section>
     )
